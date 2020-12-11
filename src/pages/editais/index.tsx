4213741c8eac0f5b3c56862300e7da1c22f6ce04
Tom Benevides <eltonfelixbenevides@gmail.com>
2020-12-11 11:40:11 -0400

    page editais: just a sample page

---
+++ src/pages/editais/index.tsx
@@ -0,0 +1,12 @@
+import LayoutLP from '../../components/Layouts/LayoutLP';
+import Home from '../../components/PageContent/home';
+
+export default function App() {
+  return (
+    <>
+      <LayoutLP>
+        <Home />
+      </LayoutLP>
+    </>
+  );
+}
